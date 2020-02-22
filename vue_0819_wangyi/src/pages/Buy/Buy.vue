a1d2af66072a12dd3b20356e3c686f96be3dbc4c
dudada <1084299433@qq.com>
2020-02-22 14:09:20 +0800

    not waterfall

--- vue_0819_wangyi/src/pages/Buy/Buy.vue
+++ vue_0819_wangyi/src/pages/Buy/Buy.vue
@@ -1,15 +1,289 @@
 <template>
-  <div>
-    buy
+  <div id="buy-container">
+      <!-- 头部区 -->
+      <div class="buy-title">
+        <div class="home"><i class="iconfont icon-fangzi"></i></div>
+        <span class="buy-span">值得买</span>
+        <div class="big"><i class="iconfont icon-icon-test"></i></div>
+        <div class="car"><i class="iconfont icon-gouwuche"></i></div>
+      </div>
+      <!-- 轮播区 -->
+      <div class="buy-swiper">
+          <div class="litel-title"><h2>值得买</h2><span>严选好物 用心生活</span></div>
+          <div class="swiper-container">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                </div>
+                <div class="swiper-slide">
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                  <a href="javascript:;">
+                    <img src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120x120" alt="">
+                    <h3>9.9超值</h3>
+                    <h4>定价直降</h4>
+                  </a>
+                </div>
+              </div>
+              <div class="swiper-pagination"></div>
+          </div>
+      </div>
+      <!-- 瀑布流区 -->
+      <div class="waterfall-container"></div>
   </div>
 </template>
 
 <script>
+  import Swiper from 'swiper'
+  import 'swiper/css/swiper.min.css'
   export default {
+    async mounted(){
+       new Swiper('.swiper-container', {
+        autoplay: false,//可选选项，自动滑动
+        pagination: {
+          el: '.swiper-pagination',
+          dynamicBullets: true,
+          dynamicMainBullets: 2
+      },
+    })
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
- 
+  #buy-container
+    width 100%
+    height 100%
+    background #eee
+    //头部
+    .buy-title
+      position relative
+      width 750px
+      height 88px
+      box-sizing border-box
+      padding  0 30px
+      display flex
+      justify-content space-between
+      border-bottom 1px #eee solid
+      line-height 88px
+      position fixed
+      top 0
+      left 0
+      background #ffffff
+      z-index 99
+      .buy-span
+        display inline-block
+        margin-left 120px
+        font-size 36px
+        color #000
+      .home
+        .icon-fangzi
+          font-size 48px
+          color #333
+      .big
+        margin-right -100px
+        .icon-icon-test
+          font-size 60px
+          color #999
+      .car
+        .icon-gouwuche
+          font-size 50px
+          color #666
+    //轮播
+    .buy-swiper
+      margin-top 88px 
+      width 750px
+      height 684px
+      background-image url("https://m.you.163.com/topic/index/img/topic_title_bg.2373a140.png")
+      background-size 100% 100%
+      .litel-title
+        h2,span
+          font-size 50px
+          color #ffffff
+          float left
+          margin 70px 0 15px 20px
+        span 
+          font-size 30px
+          line-height 70px
+      .swiper-container
+        width 710px
+        height 540px
+        background #fff
+        border-radius 20px
+        box-sizing border-box
+        a 
+          width 175px
+          height 235px
+          display inline-block
+          text-align center
+          box-sizing border-box
+          padding-top 40px
+          img 
+            width 120px
+            height 120px
+          h3
+            font-size 28px
+            font-weight bold
+            color #333
+            line-height 50px
+    .waterfall-container
+      width 710px
+      height 100px
+      margin 30px 20px 0px
 </style>
